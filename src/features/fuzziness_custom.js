fb97fb621a22ca5093fd76dcfa78449da552f3ba
Moontrick <47784395+Moontrick@users.noreply.github.com>
2023-12-04 19:28:21 +0400

    Create fuzziness_custom.js

---
+++ src/features/fuzziness_custom.js
@@ -0,0 +1,15 @@
+
+export const DATA ={
+    "syn" : [
+        {
+            "str_find": "7."     
+        },
+        {
+            "str_find": "6."
+        },
+        {
+            "str_find": "TI"
+        }  
+    ]
+}
+
